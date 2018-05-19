472c535be92acaf03f6e8af7e388981ad75d75c6
yangming <1845423761@qq.com>
2018-05-19 01:40:53 -0700

    first time to commit

---
+++ aa.java
@@ -0,0 +1,15 @@
+public class aa{
+	public static void main(String[] args){
+	int [] arr={10,20,30,50,100};
+	int max=arr[0];
+	for(int i=0;i<arr.length;i++){
+	if(max<arr[i]){
+		max=arr[i];
+ 	}
+		
+}
+
+ System.out.println("zuidazhiwei"+max);
+
+	}
+}
